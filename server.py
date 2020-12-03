04caeefc56705de2b2ec039e2b56d7a0ba84eec1
chaewon0824 <68092600+chaewon0824@users.noreply.github.com>
2020-12-04 02:19:45 +0900

    Update server.py

--- server.py
+++ server.py
@@ -1,4 +1,3 @@
-from flask import Flask,render_template,request,Response
 from socket import *
 
 
@@ -6,29 +5,27 @@ HOST = '127.0.0.1'
 PORT =  7777
 BUF_SIZE = 1024
 
-'''
-app = Flask(__name__)
-app.debug = True
-'''
+
 
 serverSock = socket(AF_INET,SOCK_STREAM)
 serverSock.bind((HOST,PORT))
 serverSock.listen()
 
-connectionSock, addr = serverSock.accept()
+connectionSock, addr = serverSock.accept() #addr = Address Family
 print(str(addr),'Success Connection\n')
 
 
-def send(sock,sendData):
+def send(sock):
 	sendData = input('>>>') #임시로 테스트를 위해 서버측에서 데이터를 콘솔로 입력하는 부분
-	sock.send(sendData.encode()) #모델에게 data를 보냄
+	sock.send(sendData.encode()) #클라이언트에게 data를 보냄
 	
 def receive(sock):
-	recvData = sock.recv(BUF_SIZE) #모델한테 data 받음
+	recvData = sock.recv(BUF_SIZE) #클라이언트한테 data 받음
 	print('Client: ',recvData.decode('utf-8')) #임시로 서버측 콘솔에 대답을 띄움
 	
 
 while True: #서버측 반복부분
-	send(connectionSock)
 	receive(connectionSock)
+	send(connectionSock)
+	
 	
